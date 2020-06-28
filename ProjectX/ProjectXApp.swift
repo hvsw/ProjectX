7cc7efd6729908ecf881c94ccd535dae7ac1b895
Henrique Valcanaia <henriqueindalencio+github@gmail.com>
2020-06-28 14:06:44 -0300

    Initial Commit

---
+++ ProjectX/ProjectXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectXApp.swift
+//  ProjectX
+//
+//  Created by Henrique Valcanaia on 2020-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ProjectXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
