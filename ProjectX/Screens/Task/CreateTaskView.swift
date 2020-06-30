a5a59859dca80aaa5d33701f98d60982edb963b0
William Marques Hass <wm.hass@gmail.com>
2020-06-30 12:43:01 +0200

    more

--- ProjectX/Screens/Task/CreateTaskView.swift
+++ ProjectX/Screens/Task/CreateTaskView.swift
@@ -7,81 +7,30 @@
 
 import SwiftUI
 
+struct CreateTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CreateTaskView()
+        }
+    }
+}
+
 struct CreateTaskView: View {
-    
     @State var taskType: Int = 0
 
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Tipo").font(.title)
-                        Spacer()
-                    }
-                    Picker(selection: $taskType,
-                           label: Text("Picker")) {
-                        Text("Sintoma").tag(0)
-                        Text("Efeito colateral").tag(1)
-                        Text("Medicação").tag(2)
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                Spacer()
-                
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Sintoma").font(.title)
-                    TextField("Sintoma", text: .constant(""))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Sugestões").font(.caption)
-                        HStack {
-                            ComorbityView(
-                                comorbity: Comorbity(
-                                    id: UUID(),
-                                    name: "Ansiedade",
-                                    severity: .high
-                                )
-                            )
-                            ComorbityView(
-                                comorbity: Comorbity(
-                                    id: UUID(),
-                                    name: "Pânico",
-                                    severity: .veryHigh
-                                )
-                            )
-                            ComorbityView(
-                                comorbity: Comorbity(
-                                    id: UUID(),
-                                    name: "Apetite",
-                                    severity: .low
-                                )
-                            )
-                            ComorbityView(
-                                comorbity: Comorbity(
-                                    id: UUID(),
-                                    name: "Dor de cabeça",
-                                    severity: .low
-                                )
-                            )
-                        }
-                    }
-                    .padding(.top, 6)
-                }
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Medicação").font(.title)
-                    TextField("Nome", text: .constant(""))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    TextField("Dosagem", text: .constant(""))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: 120)
-                    VStack {
-                        DatePicker(
-                            "Horário",
-                            selection: .constant(Date()),
-                            displayedComponents: .date)
-                    }
+            VStack(alignment: .leading, spacing: 16) {
+                TaskTypeView(taskType: $taskType)
+                switch taskType {
+                case 0:
+                    SymptonTaskOptionsView()
+                case 1:
+                    SideEffectsTaskOptionsView()
+                case 2:
+                    MedicationTaskOptionsView()
+                default:
+                    EmptyView()
                 }
                 Spacer()
             }
@@ -93,10 +42,161 @@ struct CreateTaskView: View {
     }
 }
 
-struct CreateTaskView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            CreateTaskView()
+struct TaskTypeView: View {
+    @Binding var taskType: Int
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Text("Tipo").font(.callout)
+                Spacer()
+            }
+            Picker(selection: $taskType,
+                   label: Text("Picker")) {
+                Text("Sintoma").tag(0)
+                Text("Efeito colateral").tag(1)
+                Text("Medicação").tag(2)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+    }
+}
+
+struct SymptonTaskOptionsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Monitorar sintoma").font(.callout)
+            TextField("Descrição do sintoma", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Sugestões").font(.caption)
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack(spacing: 4) {
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Ansiedade",
+                                severity: .veryLow
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Pânico",
+                                severity: .low
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Desânimo",
+                                severity: .medium
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Mania",
+                                severity: .high
+                            )
+                        )
+                    }
+                    HStack(spacing: 4) {
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Hiperatividade",
+                                severity: .veryHigh
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Desatenção",
+                                severity: .high
+                            )
+                        )
+                    }
+                }
+            }
+            .padding(.top, 6)
+        }
+    }
+}
+
+struct SideEffectsTaskOptionsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Monitorar efeito colateral").font(.callout)
+            TextField("Efeito colateral", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Sugestões").font(.caption)
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack(spacing: 4) {
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Dor de cabeça",
+                                severity: .high
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Pouco apetite",
+                                severity: .medium
+                            )
+                        )
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Palpitação",
+                                severity: .veryLow
+                            )
+                        )
+                    }
+                    HStack(spacing: 4) {
+                        ComorbityView(
+                            comorbity: Comorbity(
+                                id: UUID(),
+                                name: "Insônia",
+                                severity: .low
+                            )
+                        )
+                    }
+                }
+            }
+            .padding(.top, 6)
+        }
+    }
+}
+
+struct MedicationTaskOptionsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Uso de medicação").font(.callout)
+            TextField("Nome", text: .constant(""))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            HStack {
+                TextField("Dosagem", text: .constant(""))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 120)
+                Picker("Medida", selection: .constant(0)) {
+                    Text("mg").tag(0)
+                    Text("g").tag(1)
+                }.pickerStyle(SegmentedPickerStyle())
+                
+            }
+            HStack {
+                DatePicker(
+                    "Horário",
+                    selection: .constant(Date()),
+                    displayedComponents: .hourAndMinute)
+                    .font(.callout)
+                Image(systemName: "plus.circle")
+                    .foregroundColor(Color(.systemBlue))
+            }
         }
     }
 }
